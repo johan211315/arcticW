82144f35a09c4386343fd235e7794c892f927552
Marcel00222 <marcel.collellc@educem.net>
2025-05-08 19:24:50 +0200

    Marcel 08/05 Merch

--- ArcticW/php/merch.php
+++ ArcticW/php/merch.php
@@ -243,7 +243,6 @@
   <!-- Imagen Merch -->
   <div class="arreglachapuzas">
     <div id="merch">
-      <img src="../img/merch/gif atardecer.jpg" alt="Promocion" />
       <p>Més articles afegits</p>
       <h3>Rebaixes</h3>
       <h2>Fins a -60%</h2>
